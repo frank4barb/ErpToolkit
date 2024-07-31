997afc364c26c09a7d0a557601bbf62c30e660e9
frank4barb <frank4barb@gmail.com>
2024-07-31 23:16:39 +0200

    serv-install

--- ServiceInstall_runasAdmin.bat
+++ ServiceInstall_runasAdmin.bat
@@ -1,5 +1,11 @@
-cd C:\dhe_desktop\dev\wbs\ErpToolkit
+@echo off
+rem Memorizza il path completo del server
+set CURR_SERVER=%cd%\bin\Release\net8.0\ErpToolkit.exe
+rem Visualizza il server
+echo Il server corrente è: %CURR_SERVER%
+pause
+rem carica il server come servizio windows
 rem dotnet publish ErpToolkit.csproj -p:Configuration=Release
-rem sc create ErpToolkitService binPath="C:\dhe_desktop\dev\wbs\ErpToolkit\bin\Release\net8.0\TaskScheduler.exe" start=delayed-auto
+rem sc create ErpToolkitService binPath=%CURR_SERVER% start=delayed-auto
 SC DELETE ErpToolkitService 
 pause
